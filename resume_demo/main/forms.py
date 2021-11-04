779f497111d27162e07f3d22088731b78a4c7cfc
KyeongHwanMin <h45652@naver.com>
2021-11-04 17:12:54 +0900

    오류수정

--- resume_demo/main/forms.py
+++ resume_demo/main/forms.py
@@ -9,20 +9,12 @@ class ContactForm(forms.ModelForm):
 			'placeholder': '*Full name..',
 			'class': 'form-control'
 			}))
-<<<<<<< HEAD
 	email = forms.EmailField(max_length=254, required=True, 
-=======
-	email = forms.EmailField(max_length=254, required=True,
->>>>>>> 4c172ba2443b21df26bae70408de9b09d1957031
 		widget=forms.TextInput(attrs={
 			'placeholder': '*Email..',
 			'class': 'form-control'
 			}))
-<<<<<<< HEAD
 	message = forms.CharField(max_length=1000, required=True, 
-=======
-	message = forms.CharField(max_length=1000, required=True,
->>>>>>> 4c172ba2443b21df26bae70408de9b09d1957031
 		widget=forms.Textarea(attrs={
 			'placeholder': '*Message..',
 			'class': 'form-control',
