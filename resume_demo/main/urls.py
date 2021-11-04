162c612c663e423da89b8b7fd89501921413d4df
KyeongHwanMin <h45652@naver.com>
2021-11-04 15:53:16 +0900

    feat(models.py) : 모델 설정

--- resume_demo/main/urls.py
+++ resume_demo/main/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="home"),
-    path('contact', views.ContactView.as_view(), name="contact"),
-    path('portfolio', views.PortfolioView.as_view(), name="portfolio"),
-    path('portfolio/<slug:slug', views.PortfolioDetailViewView.as_view(), name="portfolios"),
-    path('blog/', views.BlogView.as_view(), name="blogs"),
-    path('blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
+    # path('', views.IndexView.as_view(), name="home"),
+    # path('contact', views.ContactView.as_view(), name="contact"),
+    # path('portfolio', views.PortfolioView.as_view(), name="portfolio"),
+    # path('portfolio/<slug:slug', views.PortfolioDetailViewView.as_view(), name="portfolios"),
+    # path('blog/', views.BlogView.as_view(), name="blogs"),
+    # path('blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
 ]
