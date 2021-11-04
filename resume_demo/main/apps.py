e0ee7e27979bfbb00ddaffcbdb6d25d3f5a381e2
KyeongHwanMin <h45652@naver.com>
2021-11-04 16:48:55 +0900

    refactor(models.py) : DB 설정 완료

--- resume_demo/main/apps.py
+++ resume_demo/main/apps.py
@@ -5,5 +5,5 @@ class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 
-    def ready(self):
-        import main.singals
+    # def ready(self):
+    #     import main.signals
