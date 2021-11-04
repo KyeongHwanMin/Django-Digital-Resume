6b66f3f0929e31cfc6044153954549786d020a00
KyeongHwanMin <h45652@naver.com>
2021-11-04 17:09:25 +0900

    refactor(app.py) : 오류 수정

--- resume_demo/main/apps.py
+++ resume_demo/main/apps.py
@@ -4,11 +4,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
-<<<<<<< HEAD
+
     def ready(self):
         import main.signals
-=======
 
-    # def ready(self):
-    #     import main.signals
->>>>>>> 4c172ba2443b21df26bae70408de9b09d1957031
